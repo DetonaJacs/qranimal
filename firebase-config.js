1810ebecef6b29e33727e479828addc3a5a95e4e
Jacson Costa <104026932+DetonaJacs@users.noreply.github.com>
2025-05-22 13:58:51 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,25 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
-// Certifique-se que os dados estão corretos
 const firebaseConfig = {
   apiKey: "AIzaSyDwvoBfcMcpQEoPvEril8iGbDMOviLDdxI",
   authDomain: "qranimal-c7da5.firebaseapp.com",
   projectId: "qranimal-c7da5",
-  storageBucket: "qranimal-c7da5.appspot.com", // Corrigi o domínio aqui
+  storageBucket: "qranimal-c7da5.appspot.com",
   messagingSenderId: "418822013678",
   appId: "1:418822013678:web:52355af7cb57e61fbd29ff",
   measurementId: "G-7NV3NQN1TE"
 };
 
-// Inicialize o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider };
+// Configuração opcional para sugerir email
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+
+export { auth, db, provider };
