e056f0674f2c0ca31041debd4a51e188943b5aff
Jacson Costa <104026932+DetonaJacs@users.noreply.github.com>
2025-05-22 19:02:06 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDwvoBfcMcpQEoPvEril8iGbDMOviLDdxI",
@@ -13,18 +12,13 @@ const firebaseConfig = {
   measurementId: "G-7NV3NQN1TE"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase services
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-// Configuração do provedor Google
 provider.setCustomParameters({
   prompt: 'select_account'
 });
 
-export { db, auth, provider, storage };
+export { db, auth, provider }; // Removido storage
