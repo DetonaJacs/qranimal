38c1debd9669eb7c7355ef3da266d08ec532e191
Jacson Costa <104026932+DetonaJacs@users.noreply.github.com>
2025-05-22 13:29:48 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
-// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDwvoBfcMcpQEoPvEril8iGbDMOviLDdxI",
   authDomain: "qranimal-c7da5.firebaseapp.com",
@@ -13,9 +12,14 @@ const firebaseConfig = {
   measurementId: "G-7NV3NQN1TE"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export { db, auth };
+// Configure o provedor do Google para seu email específico
+provider.setCustomParameters({
+  login_hint: 'jacson311@gmail.com' // Substitua pelo seu email
+});
+
+export { db, auth, provider };
